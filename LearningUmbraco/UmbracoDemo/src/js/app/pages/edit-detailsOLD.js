ddaa9c2d79751089fb31bc4a7d2881c504074a70
Bob He <bob@mentordigital.co.uk>
2018-08-28 12:29:27 +0100

    remove

--- LearningUmbraco/UmbracoDemo/src/js/app/pages/edit-detailsOLD.js
+++
@@ -1,9 +0,0 @@
-//const rowToEdit = document.querySelectorAll('.tabs-content__table-edit');
-
-//const editDetails = function () {
-//    for (let i = 0; i < rowToEdit.length; i++) {
-//        rowToEdit[i].addEventListener('click', formEditCancel);
-//    } 
-//}
-
-//export default editDetails;
--- LearningUmbraco/UmbracoDemo/src/js/app/services/nhs-no-validate.js
+++
@@ -1,9 +0,0 @@
-﻿import nhsNoValidation from '../../lib/services/nhs-no-validation';
-
-function nhsNoValidate() {
-    const input = document.querySelector(".nhs-number");
-    const message = "Please enter a valid NHS number";
-    return nhsNoValidation(input, message);
-}
-
-export default nhsNoValidate;
