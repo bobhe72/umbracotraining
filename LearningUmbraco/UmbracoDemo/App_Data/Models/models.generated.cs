fabee3c43130fc0ce80ce90c593b4e6352d2fcca
Bob He <bobhe72@googlemail.com>
2018-10-13 18:48:01 +0100

    adjust pages

--- LearningUmbraco/UmbracoDemo/App_Data/Models/models.generated.cs
+++ LearningUmbraco/UmbracoDemo/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "3fe83f1d577c23f9")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
