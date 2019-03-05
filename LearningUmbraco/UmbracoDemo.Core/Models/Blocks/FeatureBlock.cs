6241a97ff1df20a82cb9189c3f45751c58ea3401
Bob He <bob@mentordigital.co.uk>
2019-03-05 16:36:52 +0000

    add web block

---
+++ LearningUmbraco/UmbracoDemo.Core/Models/Blocks/FeatureBlock.cs
@@ -0,0 +1,13 @@
+﻿using RJP.MultiUrlPicker.Models;
+using Umbraco.Core.Models;
+
+namespace UmbracoDemo.Core.Models.Blocks
+{
+    public class FeatureBlock
+    {
+        public string Heading { get; set; }
+        public string Summary { get; set; }
+        public IPublishedContent Image { get; set; }
+        public Link Link { get; set; }
+    }
+}
